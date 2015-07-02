28747faec573e34eadc43707908021b13051c5af
eaton11 <sameaton11@gmail.com>
2015-07-02 07:07:18 -0600

    updates dropdown

--- src/scripts/main/3.editor.js
+++ src/scripts/main/3.editor.js
@@ -2,56 +2,84 @@ var brandDropDown = {
 	populate: function(){
 		var brandName = el("#brandName");
 		brandName.append(
-			el("+div").addClass("dropdown").append(
+
+			el("+div").addClass(["dropdown", "hide"]).append(
+
 				el.join([
-					el("+div#searchBrandsContainer").append(
-						el("+input#searchBrands").text("serch brands").attr("placeholder", "Search")
-					),
-					el("+div#recentBrands").text("recent brands"),
-					el("+div#newBrand").text("New Brand")
+					el("+div").addClass("arrow"),
+
+					el("+div").addClass("dropdownBody").append(
+
+						el.join([
+							el("+div#searchBrandsContainer").append(
+								el("+input#searchBrands").text("serch brands").attr("placeholder", "Search")
+							),
+							el("+div#recentBrands").text("recent brands"),
+							el("+div#newBrand").text("New Brand")
+						])
+
+					)
 				])
-			)
+
+			) //dropdown
+
 		);
 		console.log("lpd",localPersistentData);
 	}
 };
 
+
+// var dropdownCheck = baton(function(next, data){
+
+// })
+// .then(function(next){
+
+// })
+
+	
+
 function enableDropdowns(){
+
 	var dropdowns = {
 		brands: "closed",
 		projects: "closed"
 	}
 	
+
 	var brandsDropdown = el("#brandName").el(".dropdown")[0];
 	// var brandDropdown = el("#brandName").el(".dropdown")[0];
 
-	function openDropdown(dropdown) {
-		if(dropdowns[dropdown] === "opened") return;
-
-		baton(function(next, dropdown){
-			console.log("dropdown opened:",dropdown);
-			if(dropdown === "brands"){
-				brandsDropdown.addClass("show");
-			}
-		}).yield(dropdown)
-
-		dropdowns[dropdown] = "opened";
+	function toggleDropdown(_dropdown){
+		if(dropdowns[_dropdown] === "opened") {
+			closeDropdown(_dropdown);
+			dropdowns[_dropdown] = "closed";
+		}
+		else if(dropdowns[_dropdown] === "closed") {
+			openDropdown(_dropdown);
+			dropdowns[_dropdown] = "opened";
+		}
 	}
 
-
-	function closeDropdown(dropdown) {
-		if(dropdowns[dropdown] === "closed") return;
-
-		baton(function(next, dropdown){
-			console.log("dropdown closed:",dropdown);
-		}).yield(dropdown)
-
-		dropdowns[dropdown] = "closed";
+	function openDropdown(_dropdown) {
+		if(_dropdown === "brands"){
+			brandsDropdown.rmClass("hide");
+		}
 	}
 
 
 
-	openDropdown("brands");
+	function closeDropdown(_dropdown) {
+		if(_dropdown === "brands"){
+			brandsDropdown.addClass("hide");
+		}
+
+	}
+
+	brandName.on("click", function(){
+		toggleDropdown("brands");
+	});
+
+	// openDropdown("brands");
 	// closeDropdown("brands");
 }
 	
