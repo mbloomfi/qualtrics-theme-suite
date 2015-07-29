03cd707d545e960d187dacc6815b0c06e75999ee
Sam Eaton <sameaton11@gmail.com>
2015-07-29 12:16:43 -0600

    removes app build

--- QTS.app/Contents/Resources/app/src/main-process/00.require-modules.js
+++
@@ -1,7 +0,0 @@
-var app = require("app");
-var BrowserWindow = require("browser-window");
-var gulp = require("gulp");
-var Menu = require("menu");
-var ipc = require("ipc");
-var fs = require("fs");
-var shell = require('shelljs');
--- QTS.app/Contents/Resources/app/src/main-process/03.editor-preview-ratio.js
+++
@@ -1,9 +0,0 @@
-// This is the function that set the ratio between the editor and the preview screen
-function setEditorPreviewRatio(_newIndex) {
-  var i = global.sharedObject.menuStatus.currentEditorPreviewRatio;
-  appMenu.items[5].submenu.items[0].submenu.items[i].checked = false;
-  appMenu.items[5].submenu.items[0].submenu.items[_newIndex].checked = true;
-  global.sharedObject.menuStatus.currentEditorPreviewRatio = _newIndex;
-  mainWindow.webContents.executeJavaScript("editorPreviewBar.set("+_newIndex+"); window.dispatchEvent(new Event('resize'));");
-
-}
